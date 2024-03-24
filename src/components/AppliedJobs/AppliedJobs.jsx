e0d2f5b656d873e501eda5286df743a3f2cfa9bc
lilycm <lilychakma733@gmail.com>
2024-03-24 21:01:02 +0600

    added job details components

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../utility/localstorage";
 import './AppliedJobs.css'
 import { SlLocationPin } from "react-icons/sl";
 import { HiOutlineCurrencyDollar } from "react-icons/hi";
-
+import { FaChevronDown } from "react-icons/fa";
 
 
 
@@ -12,6 +12,7 @@ const AppliedJobs = () => {
     const jobs = useLoaderData();
 
     const [appliedjob, setAppliedJob] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
 
 
     useEffect(() => {
@@ -28,6 +29,7 @@ const AppliedJobs = () => {
                 }
             }
             setAppliedJob(jobsApplied);
+            setDisplayJobs(jobsApplied)
             // console.log(jobs, storedJobIds, jobsApplied);
         }
     }, [])
@@ -37,50 +39,59 @@ const AppliedJobs = () => {
 
     return (
         <div>
-            <div className="lg:px-20">
+            <div className="">
                 <div className="job-details">
                     <h2 className="font-man text-2xl font-extrabold">Applied jobs: {appliedjob.length}</h2>
-
-                    <details className="dropdown">
-                        <summary className="m-1 btn">open or close</summary>
-                        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li><a>Item 2</a></li>
-                        </ul>
-                    </details>
                 </div>
-                <div className="my-20">
+                <div className="lg:my-20 lg:px-20">
+                    <div className="flex justify-end mb-7">
+                        <details className="dropdown">
+                            <summary className="m-1 btn">Filter By <FaChevronDown />
+                            </summary>
+                            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                <li><a>All</a></li>
+                                <li><a>Remote</a></li>
+                                <li><a>Onsite</a></li>
+                            </ul>
+                        </details>
+                    </div>
                     {
-                        appliedjob.map(job => <>
+                        displayJobs.map(job => <>
                             <div key={job.id}>
-                                <div className="flex items-center gap-4">
+                                <div className="flex justify-between items-center gap-4 mb-6 border rounded p-7 hover:shadow-2xl">
                                     <div className="w-4/12 h-4/12 bg-[#F4F4F4] rounded p-5 flex items-center justify-center">
-                                        <img className="w-fit h-fit" src={job.logo} alt="" />
+                                        <img className="w-9/12 h-fit" src={job.logo} alt="" />
+                                    </div>
+                                    <div className="">
+                                        <div className="space-y-4">
+                                            <h2 className="text-[#474747] font-extrabold text-2xl font-man">{job.job_title}</h2>
+                                            <h4 className='font-man text-xl font-semibold text-[#474747]'>{job.company_name}</h4>
+                                            <div className='flex gap-4'>
+                                                <button className='font-man font-extrabold text-base bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-transparent bg-clip-text border border-indigo-500 rounded px-3 py-2'>{job.remote_or_onsite}</button>
+                                                <button className='font-man font-extrabold text-base bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-transparent bg-clip-text border border-indigo-500 rounded px-3 py-2'>{job.job_type}</button>
+                                            </div>
+                                            <div className='flex gap-7'>
+                                                <div className='flex gap-3'>
+                                                    <SlLocationPin className='text-4xl' />
+                                                    <p className='text-[#757575] font-man font-semibold text-xl '>{job.location}</p>
+                                                </div>
+                                                <div className='flex gap-3'>
+                                                    <HiOutlineCurrencyDollar className='text-4xl' />
+                                                    <p className='text-[#757575] font-man font-semibold text-xl '>{job.salary}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </div>
                                     <div>
-                                        <h2 className="text-[#474747] font-extrabold text-2xl font-man">{job.job_title}</h2>
-                                        <h4 className='font-man text-xl font-semibold text-[#474747] pt-7'>{job.company_name}</h4>
-                                        <div className='flex gap-4'>
-                                            <button className='font-man font-extrabold text-base bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-transparent bg-clip-text border border-indigo-500 rounded px-3 py-2'>{job.remote_or_onsite}</button>
-                                            <button className='font-man font-extrabold text-base bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-transparent bg-clip-text border border-indigo-500 rounded px-3 py-2'>{job.job_type}</button>
-                                        </div>
-                                        <div className='flex gap-7'>
-                                            <div className='flex gap-3'>
-                                                <SlLocationPin className='text-4xl' />
-                                                <p className='text-[#757575] font-man font-semibold text-xl '>{job.location}</p>
-                                            </div>
-                                            <div className='flex gap-3'>
-                                                <HiOutlineCurrencyDollar className='text-4xl' />
-                                                <p className='text-[#757575] font-man font-semibold text-xl '>{job.salary}</p>
-                                            </div>
-                                        </div>
+                                        <Link to={`/job/${job.id}`}><button className='font-man font-extrabold text-xl bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white px-3 py-4 rounded-lg'>View Details</button></Link>
                                     </div>
                                 </div>
                             </div>
                         </>)
                     }
                 </div>
-            </div> 
+            </div>
         </div>
     );
 };
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,7 +3,10 @@ import './JobDetails.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { saveJobApplication } from '../../utility/localstorage';
-
+import { SlLocationPin } from "react-icons/sl";
+import { HiOutlineCurrencyDollar } from "react-icons/hi";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 
 
@@ -15,7 +18,7 @@ const JobDetails = () => {
     const job = jobs.find(job => job.id === idInt);
     console.log(job);
 
-    const handleApplyJob = ()=>{
+    const handleApplyJob = () => {
         saveJobApplication(idInt);
 
         toast('You Have Applied Successfully!!')
@@ -28,12 +31,57 @@ const JobDetails = () => {
                 <h2 className="text-4xl font-bold font-man text-center">Job Details</h2>
             </div>
             <div className='grid md:grid-cols-4 gap-5 md:mt-14 md:px-16'>
-                <div className='md:col-span-3'></div>
-                <div className='md:col-span-1 bg-gradient-to-r from-[#7e8ffe3b] to-[#9873ff25] py-4'>
-                    <div className='px-2'>
-                        <h4 className='font-extrabold text-xl font-man text-[#1A1919]'>Job Details</h4>
+                <div className='md:col-span-3 space-y-4'>
+                    <p className='text-[#757575] font-man  leading-relaxed'><strong className='text-[#1A1919]'>Job Description:</strong>{job.job_description}</p>
+                    <p className='text-[#757575] font-man  leading-relaxed'><strong className='text-[#1A1919]'>Job Responsibility:</strong> Collaborating with cross-functional teams: UI/UX designers often work closely with other teams, including product management, engineering, and marketing, to ensure that the user interface is aligned with business and technical requirements. You will need to be able to effectively communicate your design ideas and gather feedback from other team members.</p>
+                    <div>
+                        <strong>
+                            Educational Requirements:
+                        </strong>
+                        <p className='text-[#757575]'>{job.educational_requirements}</p>
                     </div>
-                    <div className='bg-gradient-to-r from-[#7E90FE] to-[#9873FF] rounded text-white text-center py-3'>
+                    <div>
+                        <strong>
+                            Experiences:
+                        </strong>
+                        <p className='text-[#757575]'>{job.experiences}</p>
+                    </div>
+                </div>
+                <div className='md:col-span-1 rounded'>
+                    <div className='p-4 bg-gradient-to-r from-[#7e8ffe3b] to-[#9873ff25]'>
+                        <h4 className='font-extrabold text-xl font-man text-[#1A1919]'>Job Details</h4>
+                        <hr className=' border-black' />
+                        <div className='my-5 '>
+                            <div className='flex items-center justify-start gap-3'>
+                                <HiOutlineCurrencyDollar className='text-4xl' />
+                                <p className='text-[#757575]'><span className='font-man text-base font-semibold text-[#474747]'>Salary:</span> {job.salary}</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-3'>
+                                <SlLocationPin className='text-4xl' />
+                                <p className='text-[#757575]'><span className='font-man text-base font-semibold text-[#474747]'>Job Title:</span> {job.job_title}</p>
+                            </div>
+                            <p className='font-man text-xl mt-3'><strong>Contact Information</strong></p>
+
+                        </div>
+                        <hr className=' border-black' />
+                        <div className='my-5 space-y-3'>
+                            <div className='flex items-center justify-start gap-3'>
+                                <FaPhoneAlt className='text-3xl' />
+                                <p className='text-[#757575]'><span className='font-man text-base font-semibold text-[#474747]'>Phone:</span> {job.contact_information.phone}</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-3'>
+                                <MdOutlineEmail className='text-3xl' />
+                                <p className='text-[#757575]'><span className='font-man text-base font-semibold text-[#474747]'>Email:</span> {job.contact_information.email}</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-3'>
+                                <SlLocationPin className='text-4xl' />
+                                <p className='text-[#757575]'><span className='font-man text-base font-semibold text-[#474747]'>Address:</span> {job.contact_information.address}</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='mt-3 bg-gradient-to-r from-[#7E90FE] to-[#9873FF] rounded text-white text-center py-3'>
                         <button onClick={handleApplyJob}>Apply Now</button>
                     </div>
 
