b6efcd08e32d7a2b6798f9d25e898139022a8858
lilycm <lilychakma733@gmail.com>
2024-03-24 14:30:46 +0600

    added function for show all data

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,13 +4,20 @@ import { useEffect } from 'react';
 import Job from '../Job/Job';
 
 const FeaturedJobs = () => {
+
     const [jobs, setjobs] = useState([]);
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
             .then(data => setjobs(data))
     }, [])
-    console.log(jobs);
+    // console.log(jobs);
+
+
+    // this is not the best way to load data in a webpage
+    // to show only 4 data on a page
+    const [jobsDataLength, setJobsDataLength]= useState(4);
+
 
 
     return (
@@ -19,14 +26,14 @@ const FeaturedJobs = () => {
                 <h1 className="font-man text-4xl font-extrabold">Featured Jobs</h1>
                 <p className='text-[#757575] font-semibold text-base mt-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 lg:px-20 gap-6 mt-12'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 lg:px-20 gap-6 mt-12 mb-8'>
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, jobsDataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
             </div>
 
-            <div className='mt-8'>
-                <button className='font-man font-extrabold text-xl bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white px-3 py-4 rounded-lg'>See All Jobs</button>
+            <div className={jobsDataLength === jobs.length && 'hidden'}>
+                <button onClick={()=> setJobsDataLength(jobs.length)} className='font-man font-extrabold text-xl bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white px-3 py-4 rounded-lg'>See All Jobs</button>
             </div>
         </div>
     );
